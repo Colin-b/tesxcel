f333d0680e710867aaf6cef9fc85136626e5f107
Simon Le <simon.lessage@gmail.com>
2021-01-18 18:10:26 +0100

    Use Openpyxl instead of xlrd as the latter now doesn't support any file format but .xls

--- tesxcel/_excel.py
+++ tesxcel/_excel.py
@@ -1,45 +1,43 @@
 from typing import Union
-import xlrd
-from xlrd.sheet import Sheet
+import openpyxl
+from io import BytesIO
 
 
 def assert_excel_content(
     file_path_or_actual_content: Union[str, bytes], expected_file_path: str
 ):
     if isinstance(file_path_or_actual_content, str):
-        actual_workbook = xlrd.open_workbook(file_path_or_actual_content)
+        actual_workbook = openpyxl.open(file_path_or_actual_content)
     else:
-        actual_workbook = xlrd.open_workbook(file_contents=file_path_or_actual_content)
-    expected_workbook = xlrd.open_workbook(expected_file_path)
-
-    assert sorted(actual_workbook.sheet_names()) == sorted(
-        expected_workbook.sheet_names()
+        actual_workbook = openpyxl.load_workbook(BytesIO(file_path_or_actual_content))
+    expected_workbook = openpyxl.open(expected_file_path)
+    print()
+    assert sorted(actual_workbook.sheetnames) == sorted(
+        expected_workbook.sheetnames
     ), "Different sheet names"
 
-    for sheet_name in actual_workbook.sheet_names():
+    for sheet_name in actual_workbook.sheetnames:
         _assert_sheet_content(
             sheet_name,
-            actual_workbook.sheet_by_name(sheet_name),
-            expected_workbook.sheet_by_name(sheet_name),
+            actual_workbook.get_sheet_by_name(sheet_name),
+            expected_workbook.get_sheet_by_name(sheet_name),
         )
 
 
-def _assert_sheet_content(
-    sheet_name: str, actual_worksheet: Sheet, expected_worksheet: Sheet
-):
+def _assert_sheet_content(sheet_name: str, actual_worksheet, expected_worksheet):
     assert (
-        actual_worksheet.nrows == expected_worksheet.nrows
+        actual_worksheet.max_row == expected_worksheet.max_row
     ), f"Different number of rows in {sheet_name} sheet"
     assert (
-        actual_worksheet.ncols == expected_worksheet.ncols
+        actual_worksheet.max_column == expected_worksheet.max_column
     ), f"Different number of columns in {sheet_name} sheet"
 
-    for row_index, actual_row in enumerate(actual_worksheet.get_rows()):
-        expected_row = expected_worksheet.row(row_index)
+    for row_index, actual_row in enumerate(actual_worksheet.rows):
+        expected_row = expected_worksheet[row_index + 1]
         for cell_index, actual_cell in enumerate(actual_row):
             expected_cell = expected_row[cell_index]
             assert (
-                actual_cell.ctype == expected_cell.ctype
+                actual_cell.data_type == expected_cell.data_type
             ), f"Different cell type in row {row_index}, col {cell_index} in {sheet_name} sheet"
             assert (
                 actual_cell.value == expected_cell.value
